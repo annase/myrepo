d538fb27d04e686f6dc940ab0d3959667f668ec0
seibela <seibela@philnet-pc0034.philnet.ad.uni-koeln.de>
2014-10-28 10:40:57 +0100

    first commit

---
+++ employee/src/employee/App.java
@@ -0,0 +1,13 @@
+package employee;
+
+public class App {
+
+	public static void main(String[] args) {
+
+
+		Employee emp1 = new Employee();
+		emp1.printFullName("Peter", "Müller");
+
+	}
+
+}
---
+++ employee/src/employee/Employee.java
@@ -0,0 +1,15 @@
+package employee;
+
+public class Employee {
+	
+	String firstName;
+	String lastName;
+	Integer ID;
+	
+	
+	void printFullName(String firstName, String lastName){
+		
+		System.out.println(firstName + " " + lastName);
+	}
+	
+}
